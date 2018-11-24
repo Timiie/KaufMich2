9c157d2b92714bb59aea2d0a3f5bc0e71fbdb560
Tim <Tim.Jan.Lukas@gmail.com>
2018-11-24 11:12:18 +0100

    Initial Commit

---
+++ Kaufmich2Tests/Kaufmich2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Kaufmich2Tests.swift
+//  Kaufmich2Tests
+//
+//  Created by Boss on 24.11.18.
+//  Copyright © 2018 Lukas. All rights reserved.
+//
+
+import XCTest
+@testable import Kaufmich2
+
+class Kaufmich2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
